361858dbaebf7793907601ecdd927c50bd441100
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2024-10-31 19:31:28 +0300

    (db): created database module

---
+++ src/database/data-source.ts
@@ -0,0 +1,31 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import {config} from 'dotenv';
+
+import { SRC_PATH } from '../config/app.config';
+import { join } from 'path';
+import { SeederOptions } from 'typeorm-extension';
+import { MainSeeder } from './seeders/main.seeder';
+import { UserFactory } from './seeders/users.factory';
+import { User } from '../api/v1/users/entities/user.entity';
+config();
+const dbConfigFile = join(SRC_PATH, 'database', 'db.config.json');
+
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = require(dbConfigFile)[env];
+
+
+// Create a DataSource configuration
+export const dataSourceOptions: DataSourceOptions & SeederOptions = {
+  type: dbConfig.type,
+  host: dbConfig.host  || 'localhost',
+  port: +dbConfig.port || 5432,
+  username: dbConfig.username,
+  password: dbConfig.password,
+  database: dbConfig.database,
+  migrations: ['dist/database/migrations/**/*.js'],
+  entities: [User],
+  factories:[UserFactory],
+  seeds: [MainSeeder],
+};
+
+export const AppDataSource = new DataSource(dataSourceOptions);
---
+++ src/database/db.connect.ts
@@ -0,0 +1,30 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as path from 'path';
+
+import {dataSourceOptions} from './data-source';
+
+// Connect to db
+const AppDataSource = new DataSource(dataSourceOptions);
+const connectToDB = async (): Promise<void> => {
+  try {
+    await AppDataSource.initialize();
+    console.log(
+      'Connection to the database has been established successfully.',
+    );
+  } catch (error) {
+    console.error('Unable to connect to the database:', error.message);
+    throw error;
+  }
+};
+
+// Function to close the database connection
+const closeDBConnection = async (): Promise<void> => {
+  try {
+    await AppDataSource.destroy();
+    console.log('Database connection closed.');
+  } catch (error) {
+    console.error('Error closing database connection:', error.message);
+  }
+};
+
+export { connectToDB, AppDataSource, closeDBConnection };
