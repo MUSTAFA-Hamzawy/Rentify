2a51722493fe1ebe90c0cd81f28bf995c357587e
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2024-10-31 19:35:04 +0300

    (feat): Added helpers/utils

---
+++ src/common/decorators/trim-string.decorator.ts
@@ -0,0 +1,24 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+
+/**
+ * Decorator to trim string properties in an object.
+ * 
+ * @param validationOptions Optional validation options.
+ * @returns A function that registers the decorator.
+ */
+export function TrimString(validationOptions?: any) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      name: 'TrimString',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(value: string, args: ValidationArguments) {
+          args.object[propertyName] = value.trim();
+          return true;
+        },
+      },
+    });
+  };
+}
