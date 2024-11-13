c267a722c7bfc17cd765c4e1ff1e003e6656f734
Mustafa Mahmoud <hamzawy2019579@gmail.com>
2024-11-13 15:45:43 +0200

    (helper): added method to send email for any order create/update

--- src/common/modules/mailer/mailer.service.ts
+++ src/common/modules/mailer/mailer.service.ts
@@ -65,4 +65,46 @@ export class MailerService {
       throw new InternalServerErrorException(error);
     }
   }
+
+  /**
+   * Sends an email to the customer regarding their car rental order status.
+   *
+   * This email includes order details such as rental interval, total price, and current status.
+   *
+   * @param {string} email - The recipient's email address.
+   * @param {object} bodyInfo - Contains information about the order including:
+   * @returns {Promise<void>} - Resolves if the email is sent successfully, otherwise throws an error.
+   * @throws {InternalServerErrorException} - Thrown if the email could not be sent.
+   */
+  async sendOrderMail(email: string, bodyInfo: any): Promise<void> {
+    try {
+      const { rentalInterval, totalPrice, orderStatus, userName, orderId } =
+        bodyInfo;
+
+      let body = `
+      <h1>Order Update from Rentify</h1>
+      <p>Dear ${userName},</p>
+      <p>Your car rental order with id <strong>#${orderId}</strong> is <strong>${orderStatus.charAt(0).toUpperCase() + orderStatus.slice(1)}</strong>.</p>`;
+      console.log(orderStatus.toLowerCase());
+      if (orderStatus.toLowerCase() === 'pending') {
+        body += `
+      <p><strong>Order Details:</strong></p>
+      <ul>
+        <li>Rental Interval: ${rentalInterval} day(s)</li>
+        <li>Total Price: ${totalPrice}</li>
+        <li>Status: ${orderStatus.charAt(0).toUpperCase() + orderStatus.slice(1)}</li>
+      </ul>`;
+      }
+
+      await this.sendMail({
+        email,
+        subject: '[Rentify]: Your order status.',
+        body,
+        footer: `<p>Thank you for choosing Rentify. Please contact us if you have any questions regarding your order.</p>
+      <p>Best regards,<br>Rentify Team</p>`,
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
 }
